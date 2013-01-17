bea18d6f2364d7043236b44006ee273a0a9fff57
Xiaomao Chen <xiaomao5@live.com>
2013-01-17 12:35:09 -0500

    Added cli.py features into player.py.

--- player.py
+++ player.py
@@ -89,3 +89,19 @@ class Player(Parser):
                 super(Player, self).run()
         finally:
             self.queue.put(None)
+
+def main():
+    import sys
+    import codecs
+    sys.stdout = codecs.getwriter('mbcs')(sys.stdout, 'backslashreplace')
+    
+    for file in sys.argv[1:]:
+        try:
+            player = Player(open(file))
+        except IOError as e:
+            print e.message, 'on opening:', file
+        else:
+            player.run()
+
+if __name__ == '__main__':
+    main()
